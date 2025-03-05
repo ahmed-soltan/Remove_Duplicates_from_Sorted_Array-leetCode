5e094d6ab142f5e970979c303d9a4e7738e54a40
ahmed-soltan <soa548277@gmail.com>
2025-03-05 15:52:46 +0200

    solving proplems on array and strings

--- index.js
+++ index.js
@@ -198,6 +198,13 @@ var LinkedList = /** @class */ (function () {
     };
     return LinkedList;
 }());
+var list = new LinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
+list.push(1);
+list.push(4);
+console.log(list.length);
 var NodeD = /** @class */ (function () {
     function NodeD(value) {
         this.next = null;
--- index.ts
+++ index.ts
@@ -226,7 +226,19 @@ class LinkedList {
     temp.next = newNode;
     this.length++;
   }
+
+  removeDuplicates:()=>{}
 }
+
+const list = new LinkedList()
+
+list.push(1)
+list.push(2)
+list.push(3)
+list.push(1)
+list.push(4)
+
+console.log(list.length)
 
 class NodeD {
   data: number;
